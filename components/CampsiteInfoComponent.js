a6ca6112ca79b08cb941de602232c7875a2e1c79
Justin Wellar <jwoodrowwellar@gmail.com>
2022-03-02 20:59:14 -0500

    Redux adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -5,12 +5,18 @@ import { COMMENTS } from "../shared/comments";
 import { Card, Icon } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { postFavorite } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
     campsites: state.campsites,
     comments: state.comments,
+    favorites: state.favorites,
   };
+};
+
+const mapDispatchToProps = {
+  postFavorite: (campsiteId) => postFavorite(campsiteId),
 };
 
 function RenderCampsite(props) {
@@ -22,7 +28,7 @@ function RenderCampsite(props) {
         featuredTitle={campsite.name}
         image={{ uri: baseUrl + campsite.image }}
       >
-        ><Text style={{ margin: 10 }}>{campsite.description}</Text>
+        <Text style={{ margin: 10 }}>{campsite.description}</Text>
         <Icon
           name={props.favorite ? "heart" : "heart-o"}
           type="font-awesome"
@@ -66,19 +72,12 @@ function RenderComments({ comments }) {
 }
 
 class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favorite: false,
-    };
-  }
-
   static navigationOptions = {
     title: "Campsite Information",
   };
 
-  markFavorite() {
-    this.setState({ favorite: true });
+  markFavorite(campsiteId) {
+    this.props.postFavorite(campsiteId);
   }
 
   render() {
@@ -93,8 +92,8 @@ class CampsiteInfo extends Component {
       <ScrollView>
         <RenderCampsite
           campsite={campsite}
-          favorite={this.state.favorite}
-          markFavorite={() => this.markFavorite()}
+          favorite={this.props.favorites.includes(campsiteId)}
+          markFavorite={() => this.markFavorite(campsiteId)}
         />
         <RenderComments comments={comments} />
       </ScrollView>
@@ -102,4 +101,4 @@ class CampsiteInfo extends Component {
   }
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
